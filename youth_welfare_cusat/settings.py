7fa7ef04d6405fd94164dc90ccb97bf28b1618dd
VarshaShaheen <varshashaheen2003@gmail.com>
2024-08-10 21:35:20 +0530

    director updated

--- youth_welfare_cusat/settings.py
+++ youth_welfare_cusat/settings.py
@@ -115,9 +115,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files URL
